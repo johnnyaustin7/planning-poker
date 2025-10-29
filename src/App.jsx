07ab70656f2fdf2ca71542c824f56eda93c3d85e
Johnny Austin <johnnyaustindesign@gmail.com>
2025-10-29 12:12:07 -0400

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2269,12 +2269,22 @@ if (!revealed) {
       )}
 
       {/* How It Works Modal */}
-      {showHowItWorks && (
-        <HowItWorks darkMode={darkMode} onClose={() => setShowHowItWorks(false)} />
-      )}
-    </div>
-);
-}
+        {showHowItWorks && (
+          <HowItWorks darkMode={darkMode} onClose={() => setShowHowItWorks(false)} />
+        )}
+
+        <footer className="mt-6 text-center">
+          <p 
+            onClick={() => setShowReleaseNotes(true)}
+            className={`text-xs ${darkMode ? 'text-gray-500 hover:text-gray-400' : 'text-gray-400 hover:text-gray-500'} cursor-pointer underline`}
+            title="View release notes"
+          >
+            scrumptious v{APP_VERSION}
+          </p>
+        </footer>
+      </div>
+    );
+  }
 
   // Join page
   if (!hasJoined) {
